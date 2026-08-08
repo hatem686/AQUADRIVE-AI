68e0cba5584f34584793f9b355173395e7feb907
hatem686 <jwkrjwkr214@gmail.com>
2026-08-08 05:04:49 +0300

    Add SITL failover tester node for signal simulation

---
+++ aquatic_comms/sitl_failover_tester.py
@@ -0,0 +1,62 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+
+class SITLFailoverTester(Node):
+
+    def __init__(self):
+
+        super().__init__(
+            'sitl_failover_tester'
+        )
+
+        self.pub = self.create_publisher(
+            String,
+            '/comms/active_channel',
+            10
+        )
+
+        self.timer = self.create_timer(
+            4.0,
+            self.inject_signal_drop
+        )
+
+        self.step = 0
+
+    def inject_signal_drop(self):
+
+        msg = String()
+
+        if self.step % 2 == 0:
+
+            msg.data = "FIBER_OPTIC"
+
+            self.get_logger().info(
+                "[SIMULATION] Fiber Optic: STABLE"
+            )
+
+        else:
+
+            msg.data = "WIRELESS_4G_5G"
+
+            self.get_logger().warn(
+                "[SIMULATION] Fiber Optic: FAULT INJECTED!"
+            )
+
+        self.pub.publish(msg)
+
+        self.step += 1
+
+
+def main(args=None):
+
+    rclpy.init(args=args)
+
+    node = SITLFailoverTester()
+
+    rclpy.spin(node)
+
+    node.destroy_node()
+
+    rclpy.shutdown()
