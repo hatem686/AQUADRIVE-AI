a36234fa91606282abb9507e6177ca6263f4e585
hatem686 <jwkrjwkr214@gmail.com>
2026-08-08 05:26:01 +0300

    Add setup.py for aquatic_comms package
    Initial setup configuration for the aquatic_comms package, including metadata, entry points, and data files.

---
+++ launch/setup.py
@@ -0,0 +1,31 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'aquatic_comms'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='AQUADRIVE AI Team',
+    maintainer_email='user@todo.todo',
+    description='Hybrid communications manager and Pixhawk failover bridge for Autonomous Aquatic System',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'hybrid_comms_node = aquatic_comms.hybrid_comms_node:main',
+            'pixhawk_bridge = aquatic_comms.pixhawk_bridge:main',
+            'sitl_failover_tester = aquatic_comms.sitl_failover_tester:main',
+        ],
+    },
+)
